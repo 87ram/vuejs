833ceac8b17a44316e2513115c6bdd0758b492b6
h2-kurosawa <h2-kurosawa@prime-x.co.jp>
2022-10-19 20:15:13 +0900

    init

---
+++ nuxtapp/src/pages/fun/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<div>
+			<ul id="nakama">
+				<li v-for="(n, index) in nakama" :key="index" ref="elem" class="animal">{{ n }}</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Fun',
+	data: () => ({
+		nakama: ['neko', 'inu', 'ookami', 'hebi'],
+	}),
+	mounted() {
+		this.$nextTick().then(() => {
+			console.log('nextTick')
+			console.log(this.$refs['elem'])
+		})
+	},
+}
+</script>
